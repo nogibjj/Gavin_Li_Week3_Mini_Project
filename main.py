b3fd9f419bd085a84c7d1e692b5b1ea393964361
GLI81 <liguangzheng998@hotmail.com>
2023-09-13 16:14:27 -0400

    Edited main

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import polars as pl
 import matplotlib.pyplot as plt
 
 def generate_desc_stats(path: "str"):
-    df = pl.read_csv(path)
+    df = pl.read_csv(path, ignore_errors=True)
     # print(df)
     return df.median(), df.mean(), df.std()
 
 def generate_visual_for_titanic():
-    df = pl.read_csv("./resources/train.csv")
+    df = pl.read_csv("./resources/train.csv", ignore_errors=True)
     plt.histogram(df.get_column("Survived"))
     plt.show()
 
@@ -21,4 +21,5 @@ if __name__ == "__main__":
     # add_cli()
     path = "./resources/train.csv"
     print(generate_desc_stats(path))
+    generate_visual_for_titanic()
 
