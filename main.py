3b754b18f9453b9f1763cc9383370be00cbd0548
GLi81 <90353988+gli81@users.noreply.github.com>
2023-09-13 19:52:35 +0000

    Edited main && test_main

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import polars as pl
 
 def generate_desc_stats(path: "str"):
     df = pl.read_csv(path)
-    print(df)
-    return df.median()
+    # print(df)
+    return df.median(), df.mean(), df.std()
 
 
 
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
     # add_cli()
     path = "./resources/train.csv"
-    generate_desc_stats(path)
+    print(generate_desc_stats(path))
--- test_main.py
+++ test_main.py
@@ -4,8 +4,14 @@ Test goes here
 """
 
 # from mylib.calculator import add
-# from main import f
+from main import generate_desc_stats
 
-def test_f():
-    
-    assert 1 == 1
+def test_main():
+    m1, m2, s = generate_desc_stats("./resources/train.csv")
+    assert m1["Survived"][0] == 0.0
+    assert round(m2["Survived"][0],6) == 0.383838
+    assert round(s["Survived"][0],6) == 0.486592
+
+if __name__ == "__main__":
+    test_main()
+
